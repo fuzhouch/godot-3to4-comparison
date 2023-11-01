6df929d80ba0ee9445c533d7c0570757872ecf22
fuzhou.chen <fuzhouch@gmail.com>
2023-11-01 12:23:47 +0800

    [NEW] Add test results for is_on_floor_only().

--- godot3/Demo_Tilemap_OnFloor_Detection.gd
+++ godot3/Demo_Tilemap_OnFloor_Detection.gd
@@ -12,6 +12,7 @@ func _physics_process(_delta: float) -> void:
     item_velocity = $Item_Is_On_Floor_Detector1.move_and_slide(item_velocity, Vector2.UP)
     var is_on_floor_after = $Item_Is_On_Floor_Detector1.is_on_floor()
 
+
     if print_msg:
         print("[before] Item_Is_On_Floor_Detector1.is_on_floor()?", is_on_floor_before)
         print("[after] Item_Is_On_Floor_Detector1.is_on_floor()?", is_on_floor_after)
--- godot4/demo_tilemap_on_floor_detection.gd
+++ godot4/demo_tilemap_on_floor_detection.gd
@@ -6,12 +6,16 @@ func _physics_process(_delta: float) -> void:
     $Item_Is_On_Floor_Detector1.velocity.y += PhysicsServer2D.AREA_PARAM_GRAVITY
 
     var is_on_floor_before = $Item_Is_On_Floor_Detector1.is_on_floor()
+    var is_on_floor_before_only = $Item_Is_On_Floor_Detector1.is_on_floor_only()
     $Item_Is_On_Floor_Detector1.move_and_slide()
     var is_on_floor_after = $Item_Is_On_Floor_Detector1.is_on_floor()
+    var is_on_floor_after_only = $Item_Is_On_Floor_Detector1.is_on_floor_only()
 
     if print_msg:
         print("[before] Item_Is_On_Floor_Detector1.is_on_floor()?", is_on_floor_before)
         print("[after] Item_Is_On_Floor_Detector1.is_on_floor()?", is_on_floor_after)
+        print("[before] Item_Is_On_Floor_Detector1.is_on_floor_only()?", is_on_floor_before_only)
+        print("[after] Item_Is_On_Floor_Detector1.is_on_floor_only()?", is_on_floor_after_only)
         print_msg = false
 
 func _on_print_frequency_control_timer_timeout() -> void:
